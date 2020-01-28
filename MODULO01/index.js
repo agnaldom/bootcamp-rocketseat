9bed9608552c26f8e5671ebd26096642d0dc2801
Agnaldo marinho <agnaldomarinho7@gmail.com>
2020-01-27 21:01:46 -0300

    Usando Query params

--- MODULO01/index.js
+++ MODULO01/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const server = express();
 
+// Query params = ?teste=1
+// Route params = /users/1
+// Request body = { "name": "Agnaldo", "email": "agnaldomarinho7@gmail.com" }
+
 server.get('/teste', (req, res) => {
-    return res.json({ message: 'Hello World' });
+    const name = req.query.name;
+
+    return res.json({ message: `Hello ${name}` });
 })
 
 server.listen(3000);
