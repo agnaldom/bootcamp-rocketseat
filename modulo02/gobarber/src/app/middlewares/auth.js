3423c29f78adb534feeda570c5b9dba5d660130e
Agnaldo marinho <agnaldomarinho7@gmail.com>
2020-02-04 01:00:02 -0300

    Controlando a troca de senha ou email

--- modulo02/gobarber/src/app/middlewares/auth.js
+++ modulo02/gobarber/src/app/middlewares/auth.js
@@ -1,7 +1,25 @@
+import jwt from 'jsonwebtoken';
+
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
 export default (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  console.log(authHeader);
+  if (!authHeader) {
+    return res.status(401).json([ error: 'Token not provided' ]);
+  }
 
-  return next;
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json([ error: 'Token invalid' ]);
+  }
 };
